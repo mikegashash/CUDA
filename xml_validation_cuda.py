796cc5d6069fd35c0298682302e5afe36009c8f3
mikegashash <58237401+mikegashash@users.noreply.github.com>
2024-02-26 21:36:03 -0500

    Create xml_validation_cuda.py

---
+++ xml_validation_cuda.py
@@ -0,0 +1,12 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+from lxml import etree  # Assuming lxml for XML parsing on CPU
+
+# Sample XML data (replace with your actual data)
+xml_data = """
+<data>
+  <element id="1">Valid data</element>
+  <element id="2">Invalid value (100x)</element>
+  <element id="3">Valid data with number (123)</element>
+  <element id="4">Missing attribute</element>
+</data>
